65cfcae17a316837a919669e19e665e345de1599
Ubuntu <ubuntu@ip-172-31-19-156.ca-central-1.compute.internal>
2020-11-07 17:00:38 -0500

    adding a little bit front-end webpage.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request, abort, render_template
 app = Flask(__name__)
 
+# @app.route("/")
+# def hello():
+#    return "<h1 style='color:blue'>Hello There!</h1>"
+
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
