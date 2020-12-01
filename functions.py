af64be2881f2060d8b31e1f54330dbd9c8171411
Dynamite1847 <72677331+Dynamite1847@users.noreply.github.com>
2020-12-01 16:11:30 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -12,8 +12,8 @@ def delete(self, job_id):
     db_jobs.jobs.delete_one({'_id': ObjectId(job_id)})
 
 
-def apply_for_job(user_uid, job_id):
-    db_jobs.jobs.update_one({"_id": ObjectId(job_id)}, {"$set": {'employeeUid': user_uid}})
+def apply_for_job(user_uid, job_id,employeeEmail):
+    db_jobs.jobs.update_one({"_id": ObjectId(job_id)}, {"$set": {'employeeUid': user_uid,"employeeEmail":employeeEmail}})
     job_list=list(db_jobs.jobs.find({"_id": ObjectId(job_id)}))
     return job_list[0]['email']
 
